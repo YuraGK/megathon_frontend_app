ceb97816ddc0e569cd9d23c785b2f214316bbb29
YuraGK <112947522+YuraGK@users.noreply.github.com>
2024-05-10 21:49:34 +0300

    Delete environments directory

--- environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-  apiBaseUrl: 'https://23wxjt3rzi.execute-api.eu-north-1.amazonaws.com/dev',
-  wsUrl: 'wss://9is6on8088.execute-api.eu-north-1.amazonaws.com/dev',
-  cognito: {
-    userPoolId: 'eu-north-1_IA6gIYqE5',
-    clientId: '46a1tm3pfa35djvk42kh1gbjps'
-  }
-};
