799e783140032c77975de292a826107f2b0326ba
YuraGK <112947522+YuraGK@users.noreply.github.com>
2024-05-12 00:19:11 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const http = require('http');
+const path = require('path');
+const compression = require('compression');
+
 const app = express();
+app.use(compression());
 
-app.use(express.static(__dirname + '/dist/social-network'));
+app.use(express.static(__dirname + 'dist/social-network'));
 
-app.get('/*', (req,resp)=>{
-    resp.sendFile(__dirname+'/dist/social-network/index.html')
+app.get('*', (req,resp)=>{
+    resp.sendFile(__dirname+'dist/social-network/index.html')
 })
 
+const port = http.createServer(app);
+app.set('port',port);
 
-app.listen(process.env.PORT || 8080);
+const server = process.env.PORT || 8080;
+server.listen(port,()=>console.log(`server start $(port)`));
